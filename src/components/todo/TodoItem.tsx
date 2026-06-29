a52ee33e21c4ff0337a5112ec76bf9c32d2f6ed1
Yassir <atertay@gmail.com>
2026-06-29 13:30:26 +0100

    style: Modify the checkbox shape

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -12,21 +12,36 @@ const TodoItem = ({ task }: { task: TaskType }) => {
   };
 
   return (
-    <li className="mx-auto flex w-full max-w-lg animate-fadeIn items-center justify-between rounded px-2 py-1 font-medium shadow-dark dark:shadow-lightWhite">
-      <div className="flex items-center gap-2">
+    <li className="mx-auto flex w-full max-w-lg animate-fadeIn items-center justify-between rounded px-2 py-1.5 font-medium shadow-dark dark:shadow-lightWhite">
+      <label
+        htmlFor={task.id}
+        className={`flex cursor-pointer items-center gap-2 text-xs ${done ? "text-neutral-500 line-through dark:text-neutral-400" : ""}`}
+      >
         <input
           type="checkbox"
           onChange={isComplete}
           name={task.name}
           id={task.id}
+          className="peer hidden"
         />
-        <label
-          htmlFor={task.id}
-          className={`cursor-pointer text-xs ${done ? "text-neutral-500 line-through dark:text-neutral-400" : ""}`}
-        >
-          {task.name}
-        </label>
-      </div>
+        <div className="light:bg-[#e8e8e8] flex size-4 rounded-md border border-[#a2a1a833] transition peer-checked:bg-[#7152f3] dark:bg-[#212121]">
+          <svg
+            fill="none"
+            viewBox="0 0 24 24"
+            className="light:stroke-[#e8e8e8] size-4 dark:stroke-[#212121]"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M4 12.6111L8.92308 17.5L20 6.5"
+              strokeWidth="2.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            ></path>
+          </svg>
+        </div>
+
+        {task.name}
+      </label>
       <div className="flex items-center gap-2 text-light">
         <button
           type="button"
