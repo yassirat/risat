e34b9e066bc0b9135b8d6b871993bc666d199bd0
Yassir <atertay@gmail.com>
2026-07-07 08:26:15 +0100

    refactor: Change footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,24 +4,10 @@ const Footer = () => {
   const { t } = useTranslation("global");
 
   return (
-    <footer className="bg-[#30475e] px-6 py-3">
-      <div className="mx-auto flex max-w-5xl flex-col items-center justify-between gap-2 md:flex-row">
-        <p className="text-xs font-medium tracking-wide text-light">
-          {t("footer.right")}{" "}
-          <a
-            href="https://yassiraterta.netlify.app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="text-sm uppercase hover:underline"
-          >
-            yassir
-          </a>
-          {""} &copy; {new Date().getFullYear()}
-        </p>
-        <p className="text-center text-xs font-medium tracking-wide text-light">
-          {t("footer.data")}
-        </p>
-      </div>
+    <footer className="bg-stone-100 p-4 dark:bg-stone-950">
+      <p className="text-center text-xs font-medium tracking-wide dark:text-light">
+        {t("footer.data")}
+      </p>
     </footer>
   );
 };
