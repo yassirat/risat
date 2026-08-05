e12021a055e2c8bf7315931009348fd2dbb6a46a
Yassir <atertay@gmail.com>
2026-08-05 23:25:13 +0100

    refactor: Add a new tool

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -2,6 +2,7 @@ import { Link } from "@tanstack/react-router";
 import { useTranslation } from "react-i18next";
 import noteImg from "../assets/img/notes.jpg";
 import passImg from "../assets/img/pass.jpg";
+import qrImg from "../assets/img/qr.jpg";
 import todoImg from "../assets/img/todo.jpg";
 
 const Dashbord = () => {
@@ -26,6 +27,12 @@ const Dashbord = () => {
       href: "/pass-generate",
       img: passImg,
     },
+    {
+      id: 4,
+      title: `${t("dashboard.qr")}`,
+      href: "/qr-generator",
+      img: qrImg,
+    },
   ];
 
   // container z-20 mx-auto grid min-h-dvh animate-fadeIn place-content-center gap-6 px-8 lg:place-items-center
@@ -36,22 +43,25 @@ const Dashbord = () => {
         {tools.map((tool) => {
           return (
             <div
-              className="animate-fadeLeft flex w-full items-center overflow-hidden rounded-xl bg-white shadow-dark dark:bg-zinc-950 dark:shadow-lightWhite"
+              className="animate-fadeLeft flex w-full items-center overflow-hidden rounded-xl bg-white shadow-dark transition hover:scale-105 dark:bg-zinc-950 dark:shadow-lightWhite"
               key={tool.id}
             >
               <img
                 src={tool.img}
                 alt={tool.img}
                 loading="lazy"
-                className="w-1/2 object-cover"
+                className="h-32 w-1/2 object-cover"
               />
               <div className="grid w-1/2 place-items-center gap-2 px-2">
-                <h2 className="animate-fadeUp bg-gradient-to-b from-neutral-700 to-neutral-900 bg-clip-text text-center font-Fancy text-xl font-bold text-transparent dark:from-neutral-300 dark:to-neutral-200 lg:text-2xl">
-                  {tool.title}
-                </h2>
-
                 <Link
-                  className="flex items-center gap-2 text-sm font-medium text-blue-700 transition-colors duration-200 ease-in-out hover:text-blue-600 lg:text-base"
+                  to={tool.href}
+                  className="animate-fadeUp bg-gradient-to-b from-neutral-700 to-neutral-900 bg-clip-text text-center text-xl font-bold text-transparent hover:underline dark:from-neutral-300 dark:to-neutral-200 md:text-2xl lg:text-3xl"
+                >
+                  {tool.title}
+                </Link>
+
+                {/* <Link
+                  className="group flex items-center gap-2 text-sm font-medium text-blue-700 transition-colors duration-200 ease-in-out hover:text-blue-600 lg:text-base"
                   to={tool.href}
                 >
                   {t("dashboard.link")}
@@ -61,7 +71,7 @@ const Dashbord = () => {
                     viewBox="0 0 24 24"
                     strokeWidth={2.5}
                     stroke="currentColor"
-                    className="size-4"
+                    className="size-4 transition duration-200 group-hover:translate-x-1"
                   >
                     <path
                       strokeLinecap="round"
@@ -69,7 +79,7 @@ const Dashbord = () => {
                       d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3"
                     />
                   </svg>
-                </Link>
+                </Link> */}
               </div>
             </div>
           );
