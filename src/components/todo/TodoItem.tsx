969268b4aa278c73d82d749ca8e03cd5698dba40
Yassir <atertay@gmail.com>
2026-08-12 20:33:01 +0100

    style: Change text size

--- src/components/todo/TodoItem.tsx
+++ src/components/todo/TodoItem.tsx
@@ -88,14 +88,18 @@ const TodoItem = ({ task }: { task: TaskType }) => {
         </label>
       </div>
 
-      <button type="button" onClick={() => setShowBtns(!showBtns)}>
+      <button
+        type="button"
+        className="rounded p-1 transition hover:bg-gray-200 dark:hover:bg-gray-800"
+        onClick={() => setShowBtns(!showBtns)}
+      >
         <svg
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
           strokeWidth={2.5}
           stroke="currentColor"
-          className="size-6 rounded p-1 hover:bg-gray-200"
+          className="size-4"
         >
           <path
             strokeLinecap="round"
@@ -107,7 +111,7 @@ const TodoItem = ({ task }: { task: TaskType }) => {
       <AnimatePresence mode="wait">
         {showBtns && (
           <motion.div
-            className="absolute right-4 top-16 z-10 flex origin-top-right flex-col gap-2 rounded bg-gray-300 p-4 text-light"
+            className="absolute right-4 top-16 z-10 flex origin-top-right flex-col gap-2 rounded bg-gray-300/75 p-4 text-light backdrop-blur-sm dark:bg-gray-700/75"
             initial={{ opacity: 0, scale: 0 }}
             animate={{ opacity: 1, scale: 1 }}
             exit={{ opacity: 0, scale: 0 }}
@@ -132,7 +136,7 @@ const TodoItem = ({ task }: { task: TaskType }) => {
                   d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184"
                 />
               </svg>
-              <span>Copy</span>
+              <span className="text-xs">Copy</span>
             </button>
             <button
               type="button"
@@ -156,7 +160,7 @@ const TodoItem = ({ task }: { task: TaskType }) => {
                 <path d="M12 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7" />
                 <path d="M18.375 2.625a1 1 0 0 1 3 3l-9.013 9.014a2 2 0 0 1-.853.505l-2.873.84a.5.5 0 0 1-.62-.62l.84-2.873a2 2 0 0 1 .506-.852z" />
               </svg>
-              <span>Edit</span>
+              <span className="text-xs">Edit</span>
             </button>
             <button
               type="button"
@@ -183,7 +187,7 @@ const TodoItem = ({ task }: { task: TaskType }) => {
                 <path d="M3 6h18" />
                 <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
               </svg>
-              <span>Delete</span>
+              <span className="text-xs">Delete</span>
             </button>
           </motion.div>
         )}
